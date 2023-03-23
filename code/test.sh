fd6188823f16c34939f8a1fc59fc41fdd44fd65c
cadu.goncalves <cadu.goncalves@gmail.com>
2023-03-22 21:07:14 -0300

    Adjusted worflow to use correct FluentBit version

--- code/test.sh
+++ code/test.sh
@@ -7,4 +7,4 @@ docker run --rm \
       -v /var/run/docker.sock:/var/run/docker.sock:ro \
       -v /var/lib/docker/containers:/var/lib/docker/containers \
       -v $(pwd)/code/build:/my_plugin \
-      fluent/fluent-bit:1.8.4 /fluent-bit/bin/fluent-bit -e /my_plugin/flb-in_docker_stats.so -i docker_stats -o stdout
+      fluent/fluent-bit:1.8.11 /fluent-bit/bin/fluent-bit -e /my_plugin/flb-in_docker_stats.so -i docker_stats -o stdout
